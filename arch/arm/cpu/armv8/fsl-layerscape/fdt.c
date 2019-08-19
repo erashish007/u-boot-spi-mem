u-boot/u-boot

commit 4da0e52c9dc04b74d230e32d1bda5d751cc6f41f

armv8: fsl-layerscape: fix config dependency for layerscape pci code
Fixes a link error on layerscape platform, linking fails with CONFIG_PCI
set and CONFIG_PCI_LAYERSCAPE unset.

Signed-off-by: Alex Marginean <alexm.osslist@gmail.com>
Reviewed-by: Bin Meng <bmeng.cn@gmail.com>
Reviewed-by: Prabhakar Kushwaha <prabhakar.kushwaha@nxp.com>

arch/arm/cpu/armv8/fsl-layerscape/fdt.c
@@ -435,7 +435,7 @@ void ft_cpu_setup(void *blob, bd_t *bd)
 	do_fixup_by_path_u32(blob, "/sysclk", "clock-frequency",
 			     CONFIG_SYS_CLK_FREQ, 1);
 
-#ifdef CONFIG_PCI
+#ifdef CONFIG_PCI_LAYERSCAPE
 	ft_pci_setup(blob, bd);
 #endif
 

erashish007/u-boot-spi-mem

commit fb1f8f6a010c4aacdf53548d0ca2e4220590c54c

armv8: fsl-layerscape: Fix a typo of Layerscape PCIe config entry
The correct config entry is CONFIG_PCIE_LAYERSCAPE, this typo
result in skipping the fixup of Linux PCIe DT nodes.

Fixes: 4da0e52c9dc0 (armv8: fsl-layerscape: fix config dependency for layerscape pci code)
Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

arch/arm/cpu/armv8/fsl-layerscape/fdt.c
@@ -435,7 +435,7 @@ void ft_cpu_setup(void *blob, bd_t *bd)
 	do_fixup_by_path_u32(blob, "/sysclk", "clock-frequency",
 			     CONFIG_SYS_CLK_FREQ, 1);
 
-#ifdef CONFIG_PCI_LAYERSCAPE
+#ifdef CONFIG_PCIE_LAYERSCAPE
 	ft_pci_setup(blob, bd);
 #endif
 